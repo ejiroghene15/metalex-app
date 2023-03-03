68d74069e1a4ddae5db4fe2f596b04a918c40410
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2023-03-03 13:07:20 +0100

    alert component, resuable for displaying alert across the app

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,7 @@
 <!-- Danger alert -->
-<div class="alert alert-{{ $status }} alert-dismissible fade show" role="alert">
-  <b style="color: #fff">{{ $message }}</b>
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+<div class="alert bg-{{ $status }} alert-dismissible fade show" role="alert">
+  <b class="text-white">{{ $message }}</b>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" style="background: none;">
+    <i class="fe fe-x text-white"></i>
   </button>
 </div>
