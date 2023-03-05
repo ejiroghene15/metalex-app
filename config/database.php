7c17c110c4b833d3ecd65e025830e91b4d9dd88b
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2023-03-05 12:26:36 +0100

    cert

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
   'engine' => null,
   'sslmode' => 'require',
   'options' => extension_loaded('pdo_mysql') ? array_filter([
-      PDO::MYSQL_ATTR_SSL_CA => '/isrgrootx1.pem',
+      PDO::MYSQL_ATTR_SSL_CA => 'https://letsencrypt.org/certs/isrgrootx1.pem',
   ]) : [],
     ],
 
