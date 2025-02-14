00ed73f15edd393249d5a75302a68a1a6de138b6
ejiroghene15 <ejiroghene15@gmail.com>
2025-02-14 13:11:01 +0100

    Add link to publication

--- resources/views/main/components/news-updates.blade.php
+++ resources/views/main/components/news-updates.blade.php
@@ -1,29 +1,27 @@
 <div class="row justify-content-between mt-5 gap-6">
   @foreach($post as $_)
-    <article class="col-md-4 py-4 px-3 article">
-      <a class="stretched-link" href="{{route('full-article', ["article"=> $_->slug, "id" => $_->id])}}">
-        <figure>
-          <img src="{{$_->thumbnail}}" class="" alt="">
-        </figure>
+    <a class="col-md-4 py-4 px-3 article stretched-link"
+       href="{{route('full-article', ["article"=> $_->slug, "id" => $_->id])}}">
+      <figure>
+        <img src="{{$_->thumbnail}}" class="" alt="">
+      </figure>
 
-        <p class="category">{{$_->b_category->name}}</p>
+      <p class="category">{{$_->b_category->name}}</p>
 
-        <h4>{{$_->title}}</h4>
+      <h4>{{$_->title}}</h4>
 
 
-        <div class="d-flex pt-3 gap-3 align-items-center author-container mt-auto">
-          <img src="{{$_->author->avatar}}" alt="" class="rounded-circle avatar-lg me-1">
-          <div>
-            <p class="mb-1 text-dark name">{{$_->author->fullName()}}</p>
-            <span class="d-flex align-items-center date">
+      <div class="d-flex pt-3 gap-3 align-items-center author-container mt-auto">
+        <img src="{{$_->author->avatar}}" alt="" class="rounded-circle avatar-lg me-1">
+        <div>
+          <p class="mb-1 text-dark name">{{$_->author->fullName()}}</p>
+          <span class="d-flex align-items-center date">
                     <span class="time">{{date('M d, Y', strtotime($_->created_at))}}</span>
                     <i class="fa-solid fa-circle text-dark" style="font-size: 5px"></i>
                     <span class="text-dark">{{$_->readTime()}}</span>
                   </span>
-          </div>
         </div>
-      </a>
-    </article>
-
+      </div>
+    </a>
   @endforeach
 </div>
